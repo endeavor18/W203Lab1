e91c64cb65a2ee34889ff3719e71b070f4e3d77f
Eduarda Espindola <eduarda.espindola@berkeley.edu>
2018-09-22 13:42:59 -0300

    More stuff

--- Duda_Analysis.R
+++ Duda_Analysis.R
@@ -14,7 +14,7 @@
 # rates?
 #
 # First of all, we have to load our data:
-cancer.df <- read.csv("Documents/Mestrado/W203 - Stats/Lab1/W203Lab1/cancer.csv")
+cancer.df <- read.csv("cancer.csv")
 #
 # Now let us have a peek on this data:
 head(cancer.df,10)
@@ -54,3 +54,47 @@ cancer.df$PctSomeCol18_24<-NULL
 # will use the car library:
 library(car)
 scatterplotMatrix(~ deathRate + avgAnnCount + popEst2015 + PctPrivateCoverage + PctPublicCoverage + PctEmpPrivCoverage + povertyPercent + MedianAge + medIncome, data = cancer.df)
+#
+# Now we have displayed our chosen key variables, how they relate to each other
+# (no transformation applied). We will now dig deeper into the variables we are
+# mainly going to use to answer our research question. Those will be our primary
+# key variables:
+#
+#  deathRate | Our output variable
+#  avgAnnCount | 2009-2013 mean incidences per county
+#  PctPrivateCoverage | Percentage of the population with private insurance coverage
+#  PctPublicCoverage | Percentage of the population with public insurance coverage
+#  PctEmpPrivCoverage | Percentage of the population with private insurance coverage through employment
+#
+# The Death Rate is going to be our outcome variable, (we want to see how health
+# insurance coverage realtes to it). So let us start by digging deeper into it:
+summary(cancer.df$deathRate)
+# The whole range seems to be useful and with meaningful information, since it is
+# ranging from 59.7 and 362.8 deaths per 100k people.
+# 
+# Our next step now is to take a look at a histogram, to check how it is 
+# distributed:
+hist(cancer.df$deathRate,breaks = 50, main = "Histogram of Cancer Death Rates", xlab= "Cancer Death Rates", ylab = "Frequency", col = "lightyellow4")
+# The distribution is quite close to normal, but we should now dig deeper into
+# the outliers (below 90 and above 300):
+#
+# Below 90:
+cancer.df[cancer.df$deathRate < 90,]
+#
+# Observation #1942 catches the eye, because it has a much higher poverty percentage
+# than the other 3 lower outliers, and also because we have seen in the scatterplot
+# matrix that poverty is positively correlated to death rates. However, this might
+# be due to the very low number of incidences: less people with cancer naturally 
+# leads to a smaller percentage of deaths caused by cancer.
+#
+# Above 90:
+cancer.df[cancer.df$deathRate > 300,]
+# There is only one record for the maximum value, and the other variables in this
+# observation does not seem to stand out in any other way.
+#
+# Now we will analyze another primary key variable: the mean number of cancer 
+# incidences per county between 2009 and 2013:
+summary(cancer.df$avgAnnCount)
+# The maximum value catchwa our attention, mainly because it is in much higher
+# order of magnitude. Let's check the histogram for more enlightment:
+hist(cancer.df$avgAnnCount, breaks=100, , labels=TRUE)
